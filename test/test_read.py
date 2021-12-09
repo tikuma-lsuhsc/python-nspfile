761f49a1682b360c299d8bd370d8eeb3fc8b6db5
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2021-12-09 09:24:06 -0600

    fixed github action

--- test/test_read.py
+++ test/test_read.py
@@ -1,9 +1,9 @@
 from nspfile import read
 
 def test():
-    nspfile = "test/addf8.nsp"
+    testfile = "test/addf8.nsp"
 
-    fs, x, hdr, note = read(nspfile, return_header=True, return_note=True)
+    fs, x, hdr, note = read(testfile, return_header=True, return_note=True)
 
     print(fs, hdr, note)
 
